18244b84a1d7ce0966a529b790715edc34de91e2
ravisankar59 <ravipython59@gmail.com>
2015-10-29 12:38:44 +0530

    remove floder from project

--- bootstrap-master/app/template/typeahead/typeahead-match.html.js
+++
@@ -1,5 +0,0 @@
-angular.module("template/typeahead/typeahead-match.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("template/typeahead/typeahead-match.html",
-    "<a href tabindex=\"-1\" ng-bind-html=\"match.label | typeaheadHighlight:query\"></a>\n" +
-    "");
-}]);
